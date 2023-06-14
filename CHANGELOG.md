4b841c7dbdb466c9b6070891bc21be48e555dc91
Luke D. Jones <luke@ljones.dev>
2023-06-14 20:18:05 +1200

    Update zbus dep

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+# [3.1.0] - 2023-06-14
+### Changed
+- Updated zbus
+
 # [3.1.0] - 2022-12-25
 ### Changed
 - Remove useless `IntoPath` trait and methods
--- src/manager/tests.rs
+++ src/manager/tests.rs
@@ -177,9 +177,9 @@ fn can_do() {
 
     let res = manager.can_hybrid_sleep();
     assert!(res.is_ok());
-    assert_eq!(res, zbus::Result::Ok(IsSupported::NA));
+    assert_eq!(res, zbus::Result::Ok(IsSupported::Yes));
 
     let res = manager.can_hibernate();
     assert!(res.is_ok());
-    assert_eq!(res, zbus::Result::Ok(IsSupported::NA));
+    assert_eq!(res, zbus::Result::Ok(IsSupported::Yes));
 }
